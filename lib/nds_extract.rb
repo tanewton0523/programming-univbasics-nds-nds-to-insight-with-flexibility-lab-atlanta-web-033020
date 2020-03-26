ca8b0bbf7049ba7245e7b3df8377dab517d76dbe
TeMecha Newton <tanewton0523@gmail.com>
2020-03-26 20:30:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -35,49 +35,49 @@ end
 
 def movies_with_director_key(name, movies_collection)
   result = []
-  i = 0 
-  
+  i = 0
+
   while i < movies_collection.length do
     movie_data = movies_collection[i]
     result << movie_with_director_name(name, movie_data)
-    i += 1 
+    i += 1
   end
-  
+
   result
 end
 
 
 def gross_per_studio(collection)
   result = {}
-  i = 0 
-  
-  while i < collection.length 
+  i = 0
+
+  while i < collection.length
     movie = collection[i]
-    
+
     if !result[movie[:studio]]
       result[movie[:studio]] = movie[:worldwide_gross]
     else
       result[movie[:studio]] += movie[:worldwide_gross]
     end
-    i += 1 
+    i += 1
   end
-  
+
   result
-  
+
 end
 
 def movies_with_directors_set(source)
-  i = 0 
+  i = 0
   a_o_a_movies_by_dir = []
-  
+
   while i < source.length do
     dir_info_hash = source[i]
     director_name = dir_info_hash[:name]
     directors_movies = dir_info_hash[:movies]
     a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
-    i += 1 
+    i += 1
   end
-  
+
   a_o_a_movies_by_dir
 end
 
